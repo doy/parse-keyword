15be837bec36b343a54353e1d8179f0e532479cd
Jesse Luehrs <doy@tozt.net>
2013-07-30 17:30:53 -0400

    add callparser_linkable to OBJECT in the Makefile.PL (#3)
    i think this is the right thing, not quite sure

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -7,11 +7,15 @@ around _build_MakeFile_PL_template => sub {
     my $orig = shift;
     my $self = shift;
     my $tmpl = $self->$orig;
-    return $tmpl . <<'TMPL'
-use Devel::CallParser 'callparser1_h';
+    my $extra = <<'EXTRA';
+use Devel::CallParser 'callparser1_h', 'callparser_linkable';
 open my $fh, '>', 'callparser1.h' or die "Couldn't write to callparser1.h";
 $fh->print(callparser1_h);
-TMPL
+$WriteMakefileArgs{OBJECT} = join(' ', callparser_linkable);
+EXTRA
+    $tmpl =~ s/^(WriteMakefile\()/$extra\n$1/m
+        or die "Couldn't fix template";
+    return $tmpl;
 };
 
 __PACKAGE__->meta->make_immutable;
