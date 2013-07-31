c9edb90e1c0de979060fe4133248b9a974d3609c
Jesse Luehrs <doy@tozt.net>
2013-07-31 12:02:03 -0400

    apparently if we set OBJECT, we need to include all objects (#3)

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -8,10 +8,13 @@ around _build_MakeFile_PL_template => sub {
     my $self = shift;
     my $tmpl = $self->$orig;
     my $extra = <<'EXTRA';
+use Config;
 use Devel::CallParser 'callparser1_h', 'callparser_linkable';
 open my $fh, '>', 'callparser1.h' or die "Couldn't write to callparser1.h";
 $fh->print(callparser1_h);
-$WriteMakefileArgs{OBJECT} = join(' ', callparser_linkable);
+my @linkable = callparser_linkable;
+push @linkable, "Keyword$Config{obj_ext}" if @linkable;
+$WriteMakefileArgs{OBJECT} = join(' ', @linkable) if @linkable;
 EXTRA
     $tmpl =~ s/^(WriteMakefile\()/$extra\n$1/m
         or die "Couldn't fix template";
