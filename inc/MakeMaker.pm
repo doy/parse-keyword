3549615dfe7574885f8c4a56a40d4a4fdb7b11e2
Jesse Luehrs <doy@tozt.net>
2013-07-31 15:36:42 -0400

    let's try this ordering (#3)

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -13,7 +13,7 @@ use Devel::CallParser 'callparser1_h', 'callparser_linkable';
 open my $fh, '>', 'callparser1.h' or die "Couldn't write to callparser1.h";
 $fh->print(callparser1_h);
 my @linkable = callparser_linkable;
-push @linkable, "Keyword$Config{obj_ext}" if @linkable;
+unshift @linkable, "Keyword$Config{obj_ext}" if @linkable;
 $WriteMakefileArgs{OBJECT} = join(' ', @linkable) if @linkable;
 EXTRA
     $tmpl =~ s/^(WriteMakefile\()/$extra\n$1/m
